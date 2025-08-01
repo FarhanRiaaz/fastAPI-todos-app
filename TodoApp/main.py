baeadd43d87cb573fe5a0f7929186c8304cb464a
farhaanwirk <farhan.riaz@mrs-electronic.com>
2025-08-01 12:29:50 +0500

    feat(crud): implemented complete crud operation for todo appp

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,8 +1,64 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, Path
 from Model import models
-from Database.database import engine
+from Model.models import Todos
+from RequestModel.todo_request import TodoRequest
+from starlette import status
+from typing import Annotated
+from sqlalchemy.orm import Session
+from Database.database import engine, SessionLocal
  
 app = FastAPI()
  
 models.Base.metadata.create_all(bind=engine)
- 
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
+database_injection = Annotated[Session,Depends(get_db)]
+
+@app.get("/",status_code= status.HTTP_200_OK)
+async def read_all(db: database_injection):
+    return db.query(Todos).all()  
+
+@app.get("/todo/{todo_id}",status_code= status.HTTP_200_OK)
+async def read_todo(db: database_injection, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id== todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    else:
+        raise HTTPException(status_code=404,detail='Todo item not found!')
+    
+@app.post("/todo",status_code=status.HTTP_201_CREATED)
+async def add_todo(db: database_injection,todo_request: TodoRequest):
+    todo_model= Todos(**todo_request.model_dump())
+    db.add(todo_model)
+    db.commit()
+    
+@app.put("/todo/{todo_id}",status_code=status.HTTP_204_NO_CONTENT)
+async def update_todo(db: database_injection,
+                      todo_request: TodoRequest,
+                      todo_id: int= Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=404,detail= 'Item not found for id {todo_id}!')
+    else:
+        todo_model.title = todo_request.title
+        todo_model.description = todo_request.description
+        todo_model.priority = todo_request.priority
+        todo_model.complete = todo_request.complete
+        
+        db.add(todo_model)
+        db.commit()
+        
+@app.delete("/todo/{todo_id}",status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(db: database_injection, todo_id:int = Path(gt=0)):
+            todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+            if todo_model is None:
+                raise HTTPException(status_code=404,detail='Todo not found!')
+            else:
+                db.query(Todos).filter(Todos.id == todo_id).delete()
+                db.commit()
