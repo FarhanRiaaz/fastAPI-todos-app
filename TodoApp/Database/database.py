96ddb6a41ae176cc3ab069330baa0ea5a0ce2620
farhaanwirk <farhan.riaz@mrs-electronic.com>
2025-08-01 16:56:40 +0500

    feat(users): added user table to the database created new database

--- TodoApp/Database/database.py
+++ TodoApp/Database/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
  
-SQLAlCHEMY_DATABASE_URL= 'sqlite:///./todos.db'
+SQLAlCHEMY_DATABASE_URL= 'sqlite:///./todosapp.db'
  
 engine = create_engine(SQLAlCHEMY_DATABASE_URL,connect_args={'check_same_thread':False})
  
