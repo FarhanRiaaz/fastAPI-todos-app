535c386af99d3f52201320d1479e88b19518a242
farhaanwirk <farhan.riaz@mrs-electronic.com>
2025-08-02 09:56:26 +0500

    feat(jwt): implemented the jwt token flow for authentication

--- TodoApp/Routers/auth.py
+++ TodoApp/Routers/auth.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta, timezone
 from typing import Annotated
 from fastapi import APIRouter, Depends
 from RequestModel.user_request import CreateUserRequest
@@ -7,8 +8,13 @@ from starlette import status
 from Database.database import SessionLocal
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordRequestForm
+from jose import jwt
+from ResponseModel.token import Token
 
 router = APIRouter()
+
+SECRET_KEY = 'a905b8123ae530eb75a95473f178fb191afa58436f9ae5865fd01fd9985d5977'
+ALGORITHM = 'HS256'
 
 brcypt_context = CryptContext(schemes=['bcrypt'],deprecated = 'auto')
 
@@ -27,8 +33,14 @@ def authenticate_user(username: str, passowrd: str,db):
         return False
     if not brcypt_context.verify(passowrd,user.hashed_password):
         return False
-    return True
-    
+    return user
+
+def create_access_token(user_name: str, user_id: str, expires_delta: timedelta):
+    encode= {'sub':user_name,'id':user_id}
+    expires= datetime.now(timezone.utc) + expires_delta
+    encode.update({'exp':expires})
+    return jwt.encode(encode,SECRET_KEY,algorithm=ALGORITHM)
+        
 
 @router.post("/add_user",status_code=status.HTTP_201_CREATED)
 async def create_user(db: database_injection, create_user_request: CreateUserRequest):
@@ -46,10 +58,11 @@ async def create_user(db: database_injection, create_user_request: CreateUserReq
     db.add(create_user_model)
     db.commit()
 
-@router.post("/token",status_code=status.HTTP_202_ACCEPTED)
+@router.post("/token",status_code=status.HTTP_202_ACCEPTED,response_model=Token)
 async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],db: database_injection):
     user = authenticate_user(form_data.username,form_data.password,db)
     if not user:
         return 'Failed Authentication!'
     else:
-        return 'Sucessfull Authenctication!'
+        token = create_access_token(user_name=user.username,user_id=user.id,expires_delta=timedelta(20))    
+        return {'access_token':token,'token_type':'bearer'}
