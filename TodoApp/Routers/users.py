115aaa30212e86717421ac6729ed4b90e9cbd328
farhaanwirk <farhan.riaz@mrs-electronic.com>
2025-08-04 12:42:07 +0500

    refactor: updated authentication APIs

--- TodoApp/Routers/users.py
+++ TodoApp/Routers/users.py
@@ -3,13 +3,15 @@ from fastapi import APIRouter, Depends,HTTPException
 from starlette import status
 from Model.models import Users
 from sqlalchemy.orm import Session
-from .auth import get_current_user,authenticate_user,brcypt_context
+from passlib.context import CryptContext
+from RequestModel.user_verification_request import UserVerification
+from .auth import get_current_user,authenticate_user
 
 from Database.database import SessionLocal
 
 router = APIRouter(
-    prefix='/users',
-    tags=['users']
+    prefix='/user',
+    tags=['user']
 )
 def get_db():
     db = SessionLocal()
@@ -20,33 +22,25 @@ def get_db():
         
 database_injection = Annotated[Session,Depends(get_db)]
 user_dependency = Annotated[dict,Depends(get_current_user)]
+brcypt_context = CryptContext(schemes=['bcrypt'],deprecated = 'auto')
 
 @router.get("/get_user",status_code=status.HTTP_200_OK)
 async def get_user(db: database_injection, user: user_dependency):
     if user is None:
         raise HTTPException(status_code=401,detail='Authentication Failed!')
-    user_profile = db.query(Users).filter(Users.id == user.get('id')).first()
-    if user_profile is None:
-        raise HTTPException(status_code=404,detail='User profile not found!')
-    else:
-        return user_profile
-
+    return db.query(Users).filter(Users.id == user.get('id')).first()
     
 @router.put("/update_passowrd",status_code=status.HTTP_204_NO_CONTENT)
-async def update_password(db:database_injection,user:user_dependency,old_password:str, new_password: str):
+async def update_password(db:database_injection,user:user_dependency,user_verification: UserVerification):
     if user is None:
         raise HTTPException(status_code=401,detail='Authentication Failed!')
     current_user = db.query(Users).filter(Users.id == user.get('id')).first()
-    if current_user is None:
-        raise HTTPException(status_code=404,detail='User not found!')
+    if not brcypt_context.verify(user_verification.password,current_user.hashed_password):
+        raise HTTPException(status_code=401,detail='Password didnot matched!')
     else:
-        user = authenticate_user(current_user.username,old_password,db)
-        if not user:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail='Password didnnot matched!')
-        else:
-            user.hashed_password = brcypt_context.hash(new_password)
-            db.add(user)
-            db.commit()
+        current_user.hashed_password = brcypt_context.hash(user_verification.password)
+        db.add(current_user)
+        db.commit()
         
     
         
