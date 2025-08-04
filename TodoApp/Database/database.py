be6e08f23e2f1db242a534be9b0f6692b503c8eb
farhaanwirk <farhan.riaz@mrs-electronic.com>
2025-08-04 16:15:21 +0500

    feat(database): added configrations for postgresql and mysql database

--- TodoApp/Database/database.py
+++ TodoApp/Database/database.py
@@ -1,10 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
- 
-SQLAlCHEMY_DATABASE_URL= 'sqlite:///./todosapp.db'
- 
-engine = create_engine(SQLAlCHEMY_DATABASE_URL,connect_args={'check_same_thread':False})
+#This setup is for sqlite database that is locally built and ready to use version 
+#SQLAlCHEMY_DATABASE_URL= 'sqlite:///./todosapp.db'
+#engine = create_engine(SQLAlCHEMY_DATABASE_URL,connect_args={'check_same_thread':False})
+
+#This setup is for MYSQL database and it's configurations are as follow:
+#SQLAlCHEMY_DATABASE_URL= 'myaql+pymysql://root:password@127.0.0.1:3306/TodoApplicationDatabase'
+#engine = create_engine(SQLAlCHEMY_DATABASE_URL)
+
+#This setup is for POSTGRESS database and it's configurations are as follow:
+SQLAlCHEMY_DATABASE_URL= 'postgresql://postgres:@localhost/TodoApplicationDatabase'
+engine = create_engine(SQLAlCHEMY_DATABASE_URL)
  
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
  
