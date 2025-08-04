2df73841628e5df8e1ac2a2e7354a4a33aaf926a
farhaanwirk <farhan.riaz@mrs-electronic.com>
2025-08-04 18:33:12 +0500

    test(basic): added basic test

---
+++ TodoApp/test/test_example.py
@@ -0,0 +1,19 @@
+def test_equal_or_not_equal():
+    assert 3 == 3
+    
+def test_is_instance():
+    assert isinstance('this is a string',str)
+    
+def test_boolean():
+    validated = True
+    assert validated is True
+    assert ('hello' == 'world') is False   
+
+def test_type():
+    assert type('hello' is str)
+    assert type('world' is not int)
+
+
+def test_ranges():
+    assert 5>4
+    assert 4>1
