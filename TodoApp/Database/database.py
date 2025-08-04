13e96cdf7396648bee7b091f08bb70b8578a2364
farhaanwirk <farhan.riaz@mrs-electronic.com>
2025-08-04 18:16:25 +0500

    fix(database): update path for MySQL

--- TodoApp/Database/database.py
+++ TodoApp/Database/database.py
@@ -6,7 +6,7 @@ SQLAlCHEMY_DATABASE_URL= 'sqlite:///./todosapp.db'
 engine = create_engine(SQLAlCHEMY_DATABASE_URL,connect_args={'check_same_thread':False})
 
 #This setup is for MYSQL database and it's configurations are as follow:
-#SQLAlCHEMY_DATABASE_URL= 'myaql+pymysql://root:password@127.0.0.1:3306/TodoApplicationDatabase'
+#SQLAlCHEMY_DATABASE_URL= 'myaql+pymysql://root:password@127.0.0.1:61062/TodoApplicationDatabase'
 #engine = create_engine(SQLAlCHEMY_DATABASE_URL)
 
 #This setup is for POSTGRESS database and it's configurations are as follow:
