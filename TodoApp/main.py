dd7b75aa3b3767f62bdc6af17efde9f6ccbd2f13
farhaanwirk <farhan.riaz@mrs-electronic.com>
2025-08-06 23:53:13 +0500

    chore: added setup for frontend connection

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,12 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from .models import Base
 from .database import engine, SessionLocal
+from fastapi.templating import Jinja2Templates
 # this line will import the routers from there
 from .routers import auth,todos,users
 app = FastAPI()
  
 
 Base.metadata.create_all(bind=engine)
+templates = Jinja2Templates(directory='TodoApp/templates')
+
+@app.get("/")
+def test(request: Request):
+    return templates.TemplateResponse("home.html",{"request":request})
+
 #this is the health check to check if the app work or not
 @app.get("/healthy")
 def health_check():
